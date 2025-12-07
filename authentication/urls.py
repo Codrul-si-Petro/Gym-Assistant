a22e710502929567acff257247cd134697a115d3
Codrul <codreanu.andrei1125@gmail.com>
2025-12-07 23:12:53 +0200

    add endpoint to see current logged in user

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import (
         login_success_view,
-        login_page_view
+        login_page_view,
+        current_user
         )
 
 urlpatterns = [
     path('login-success/', login_success_view, name='login_success'),
-    path('login/', login_page_view, name='login')
+    path('login/', login_page_view, name='login'),
+    path('auth/current-user-logged-in', current_user)
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,7 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import login
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 
@@ -19,3 +21,14 @@ def login_page_view(request):
             messages.error(request, "Invalid username or password")
 
     return render(request, "auth/login.html", {"form": form})
+
+
+@api_view(["GET"])
+def current_user(request):
+    if request.user.is_authenticated:
+        return Response({
+            'username': request.user.username,
+            'email': request.user.email,
+            'id': request.user.id
+            })
+    return Response(None)
