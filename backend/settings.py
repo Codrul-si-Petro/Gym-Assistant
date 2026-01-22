db6c6d724ed8fbc98ad230af284c056f8e94d6f4
Codrul <codreanu.andrei1125@gmail.com>
2026-01-22 08:50:37 +0200

    fix google auth

--- backend/settings.py
+++ backend/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
+    "django.contrib.sites",  # Required for allauth
     "django.contrib.staticfiles",
     "django.contrib.messages",
 ]
