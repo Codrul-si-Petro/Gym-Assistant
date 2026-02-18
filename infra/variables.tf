90d99bcbce911fbba4a9a3cd3f6f20daec0b24cd
Codrul <codreanu.andrei1125@gmail.com>
2026-02-19 00:33:10 +0200

    infra/add-gh-secrets-as-iac

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,3 +3,74 @@ variable "GITHUB_TOKEN" {
   sensitive = true
   # No default - must be set in TFC or via TF_VAR_GITHUB_TOKEN
 }
+
+# --- GitHub Actions secrets ---
+variable "DATABASE_URL" {
+  type      = string
+  sensitive = true
+}
+
+variable "DATABASE_URL_NO_POOLER" {
+  type      = string
+  sensitive = true
+}
+
+variable "DBT_DBNAME" {
+  type      = string
+  sensitive = true
+}
+
+variable "DBT_HOST" {
+  type      = string
+  sensitive = true
+}
+
+variable "DBT_PASSWORD" {
+  type      = string
+  sensitive = true
+}
+
+variable "DBT_USER" {
+  type      = string
+  sensitive = true
+}
+
+variable "DJANGO_DEBUG" {
+  type      = string
+  sensitive = true
+}
+
+variable "DJANGO_SECRET_KEY" {
+  type      = string
+  sensitive = true
+}
+
+variable "MAILERSEND_API_TOKEN" {
+  type      = string
+  sensitive = true
+}
+
+variable "MAILERSEND_FROM_EMAIL" {
+  type      = string
+  sensitive = true
+}
+
+variable "OAUTH_CLIENT_ID" {
+  type      = string
+  sensitive = true
+}
+
+variable "OAUTH_SECRET_KEY" {
+  type      = string
+  sensitive = true
+}
+
+variable "UI_TESTER_PASS" {
+  type      = string
+  sensitive = true
+}
+
+variable "UI_TESTER_USERNAME" {
+  type      = string
+  sensitive = false
+}
