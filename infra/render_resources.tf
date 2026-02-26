007234ded4071d503655886237775c9dde943c3a
Codrul <codreanu.andrei1125@gmail.com>
2026-02-27 01:41:58 +0200

    try to fix ggh

--- infra/render_resources.tf
+++ infra/render_resources.tf
@@ -1,15 +1,14 @@
-resource "render_web_service" "backend" {
+resource "render_service" "backend" {
   name = "Gym Assistant Backend"
-  plan = "free"
-  region = "frankfurt"
+  repo = "https://github.com/Codrul-si-Petro/Gym-Assistant"
+  type = "web_service"
 
+  web_service_details = {
+    env    = "python"
+    region = "frankfurt"
+    plan   = "free"
 
-  runtime_source  {
-    native_runtime = {
-      repo_url = "https://github.com/Codrul-si-Petro/Gym-Assistant"
-      branch = "main"
-      runtime = "python"
-      auto_deploy = false
+    native = {
       build_command = "uv sync && uv run python manage.py collectstatic --noinput"
       start_command = "gunicorn backend.wsgi:application"
     }
