fb19a321277cec5218f683b9ed85d4d05087be81
Codrul <codreanu.andrei1125@gmail.com>
2026-03-04 23:11:48 +0200

    infra/add-render-secrets

--- infra/render_resources.tf
+++ infra/render_resources.tf
@@ -17,6 +17,23 @@ resource "render_web_service" "backend" {
     runtime       = "python"
   }
 
+  env_vars = {
+    DATABASE_URL          = var.DATABASE_URL
+    DATABASE_URL_NO_POOLER = var.DATABASE_URL_NO_POOLER
+    DBT_DBNAME            = var.DBT_DBNAME
+    DBT_HOST              = var.DBT_HOST
+    DBT_PASSWORD          = var.DBT_PASSWORD
+    DBT_USER              = var.DBT_USER
+    DJANGO_DEBUG          = var.DJANGO_DEBUG
+    DJANGO_SECRET_KEY     = var.DJANGO_SECRET_KEY
+    MAILERSEND_API_TOKEN  = var.MAILERSEND_API_TOKEN
+    MAILERSEND_FROM_EMAIL = var.MAILERSEND_FROM_EMAIL
+    OAUTH_CLIENT_ID       = var.OAUTH_CLIENT_ID
+    OAUTH_SECRET_KEY      = var.OAUTH_SECRET_KEY
+    UI_TESTER_PASS        = var.UI_TESTER_PASS
+    UI_TESTER_USERNAME    = var.UI_TESTER_USERNAME
+  }
+
   lifecycle {
     ignore_changes = [
       active_custom_domains,
