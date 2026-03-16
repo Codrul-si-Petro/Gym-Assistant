c189501f051f7e90128ddf400e69a6f1d36b39ef
Codrul <codreanu.andrei1125@gmail.com>
2026-03-16 02:56:16 +0200

    infra: refactor local vars

--- infra/secrets.tf
+++ infra/secrets.tf
@@ -1,30 +1,10 @@
-locals {
-  shared_secrets = {
-    DATABASE_URL           = var.DATABASE_URL
-    DATABASE_URL_NO_POOLER = var.DATABASE_URL_NO_POOLER
-    DBT_DBNAME             = var.DBT_DBNAME
-    DBT_HOST               = var.DBT_HOST
-    DBT_PASSWORD           = var.DBT_PASSWORD
-    DBT_USER               = var.DBT_USER
-    DJANGO_DEBUG           = var.DJANGO_DEBUG
-    DJANGO_SECRET_KEY      = var.DJANGO_SECRET_KEY
-    MAILERSEND_API_TOKEN   = var.MAILERSEND_API_TOKEN
-    MAILERSEND_FROM_EMAIL  = var.MAILERSEND_FROM_EMAIL
-    OAUTH_CLIENT_ID        = var.OAUTH_CLIENT_ID
-    OAUTH_SECRET_KEY       = var.OAUTH_SECRET_KEY
-    UI_TESTER_PASS         = var.UI_TESTER_PASS
-    UI_TESTER_USERNAME     = var.UI_TESTER_USERNAME
-    FRONTEND_URL          = var.FRONTEND_URL_DEV
-    BACKEND_URL           = var.BACKEND_URL_DEV
-  }
-  db_password             = data.doppler_secrets.gym-assistant.map.DATABASE_URL
-  MONOREPO                = "Gym-Assistant"
-}
-
-
 data "doppler_secrets" "gym-assistant" {
   project = "gym-assistant"
   config = "prd_gym-assistant"
+}
+
+locals {
+  db_password = data.doppler_secrets.gym-assistant.map.DATABASE_URL
 }
 
 # to test this for now to check if I can see it in tf cloud
