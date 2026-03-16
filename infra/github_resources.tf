282a3f156cc1120fcd0b2e7d83a6fc009e8188f5
Codrul <codreanu.andrei1125@gmail.com>
2026-03-16 23:56:15 +0200

    let doppler handle secrets entirely

--- infra/github_resources.tf
+++ infra/github_resources.tf
@@ -9,25 +9,4 @@ resource "github_repository_environment" "prod" {
   environment = "prod"
 }
 
-# these loop over doppler secrets
-resource "github_actions_environment_secret" "dev_secrets" {
-  for_each = data.doppler_secrets.dev.map
-
-  repository = "Gym-Assistant"
-  environment = github_repository_environment.dev.environment
-  secret_name = each.key
-  plaintext_value = each.value
-
-}
-
-
-resource "github_actions_environment_secret" "prod_secrets" {
-  for_each = data.doppler_secrets.prod.map
-
-  repository = "Gym-Assistant"
-  environment = github_repository_environment.prod.environment
-  secret_name = each.key
-  plaintext_value = each.value
-
-}
 
--- infra/secrets.tf
+++ infra/secrets.tf
@@ -8,3 +8,25 @@ data "doppler_secrets" "dev" {
   config = "dev"
 }
 
+
+# these loop over doppler secrets
+resource "github_actions_environment_secret" "dev_secrets" {
+  for_each = data.doppler_secrets.dev.map
+
+  repository = "Gym-Assistant"
+  environment = github_repository_environment.dev.environment
+  secret_name = each.key
+  plaintext_value = each.value
+
+}
+
+
+resource "github_actions_environment_secret" "prod_secrets" {
+  for_each = data.doppler_secrets.prod.map
+
+  repository = "Gym-Assistant"
+  environment = github_repository_environment.prod.environment
+  secret_name = each.key
+  plaintext_value = each.value
+
+}
