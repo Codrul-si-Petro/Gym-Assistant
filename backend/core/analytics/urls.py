015e775f5a5c97ce6b69098cb385928c29c94820
Codrul <codreanu.andrei1125@gmail.com>
2026-03-23 03:35:56 +0200

    [backend] - add total volume per exercise endpoint

--- backend/core/analytics/urls.py
+++ backend/core/analytics/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import FavouriteExercisesView, UserRestDaysView
+from .views import FavouriteExercisesView, TotalVolumeView, UserRestDaysView
 
 urlpatterns = [
     path("v1/rest-days", UserRestDaysView.as_view(), name="rest_days"),
     path("v1/favourite-exercises", FavouriteExercisesView.as_view(), name="favourite_exercises"),
+    path("v1/total-volume", TotalVolumeView.as_view(), name="total-volume"),
 ]
