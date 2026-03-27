ee28cc320828db7fd6432507704fb13874769d3c
Codrul <codreanu.andrei1125@gmail.com>
2026-03-28 01:17:19 +0200

    fix total volume calculation

--- backend/core/analytics/sql/get_total_volume.sql
+++ backend/core/analytics/sql/get_total_volume.sql
@@ -1,22 +1,20 @@
 WITH pre_agg AS (
-SELECT 
+SELECT
   fw.exercise_id,
-  SUM(fw.repetitions) AS total_reps,
-  SUM(load) AS total_load
+  SUM(
+    fw.repetitions::numeric * fw.load::numeric *
+    CASE WHEN fw.unit = 'LBS' THEN 0.45359237 ELSE 1 END
+  ) AS total_volume_kg
 FROM core.fact_workouts fw
 WHERE fw.user_id = %(user_id)s
   AND (%(start_date)s IS NULL OR fw.date_id >= %(start_date)s::date)
   AND (%(end_date)s IS NULL OR fw.date_id <= %(end_date)s::date)
-GROUP BY fw.exercise_id 
+GROUP BY fw.exercise_id
 )
 SELECT
   fw.exercise_id,
   e.exercise_name,
-  m.muscle_name,
-  fw.total_reps * fw.total_load AS total_volume_kg
-FROM pre_agg fw 
-JOIN core.dim_exercises e ON fw.exercise_id = e.exercise_id  
-JOIN core.exercise_muscle_bridge em ON e.exercise_id = em.exercise_id 
-JOIN core.dim_muscles m ON em.muscle_id = m.muscle_id
-WHERE em.muscle_role = 'Primary'
-ORDER BY total_volume_kg DESC
+  fw.total_volume_kg
+FROM pre_agg fw
+JOIN core.dim_exercises e ON fw.exercise_id = e.exercise_id
+ORDER BY fw.total_volume_kg DESC
