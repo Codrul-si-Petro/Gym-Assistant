ca247a77f757853c9503f51fe3dc5872f24bde9e
Codrul <codreanu.andrei1125@gmail.com>
2026-03-27 23:41:31 +0200

    add is_leaf column to models precalculated

--- db/transformation/models/dim_equipment.sql
+++ db/transformation/models/dim_equipment.sql
@@ -1,4 +1,9 @@
 SELECT 
-  *,
+  e.*,
+  NOT EXISTS (
+    SELECT 1
+    FROM {{ ref('seed_equipment') }} c
+    WHERE c.equipment_parent_id = e.equipment_id
+  ) AS is_leaf,
   NOW() AS last_built
-FROM {{ ref('seed_equipment') }}
+FROM {{ ref('seed_equipment') }} e
--- db/transformation/models/dim_exercises.sql
+++ db/transformation/models/dim_exercises.sql
@@ -1,4 +1,9 @@
 SELECT 
-  *,
+  e.*,
+  NOT EXISTS (
+    SELECT 1
+    FROM {{ ref('seed_exercises') }} c
+    WHERE c.exercise_parent_id = e.exercise_id
+  ) AS is_leaf,
   NOW() AS last_built
-FROM {{ ref('seed_exercises') }}
+FROM {{ ref('seed_exercises') }} e
