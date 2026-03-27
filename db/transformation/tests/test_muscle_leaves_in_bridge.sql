c0d99581465948641699df0c9c026963fb0e9fbe
Codrul <codreanu.andrei1125@gmail.com>
2026-03-28 01:24:35 +0200

    fix bad reference in dbt test

--- db/transformation/tests/test_muscle_leaves_in_bridge.sql
+++ db/transformation/tests/test_muscle_leaves_in_bridge.sql
@@ -1,13 +1,13 @@
 -- check all muscle ids in bridge table exist as a leaf node in the muscle table
 WITH leaf_nodes AS (
   SELECT m.muscle_id
-  FROM core.dim_muscles m
-  LEFT JOIN core.dim_muscles c
+  FROM {{ ref('seed_muscles') }} m
+  LEFT JOIN {{ ref('seed_muscles') }} c
     ON m.muscle_id = c.muscle_parent_id::INT
   WHERE c.muscle_id IS NULL
 )
 SELECT DISTINCT em.muscle_id
-FROM core.exercise_muscle_bridge em
+FROM {{ ref('seed_exercise_muscles') }} em
 WHERE NOT EXISTS (
     SELECT 1
     FROM leaf_nodes l
