fdf6bdfa4e252bd91cc56dac28ea53cb5e53832a
Codrul <codreanu.andrei1125@gmail.com>
2026-04-04 23:52:15 +0300

    [DB] - fix hierarchy model

--- db/transformation/models/hierarchies/dimension_hierarchies.sql
+++ db/transformation/models/hierarchies/dimension_hierarchies.sql
@@ -1,24 +1,25 @@
 {{ config(
     materialized='table',
-    post_hook=create_indexes('core.dimension_hierarchies', [['ancestor_id', 'dimension']])
+    post_hook=create_indexes('core.dimension_hierarchies', [['current_id', 'dimension']])
 ) }}
 
 WITH RECURSIVE exercise_tree AS (
     SELECT
         'exercise' AS dimension,
-        e.exercise_name AS ancestor_name,
-        e.exercise_id AS ancestor_id,
+        e.exercise_name AS current_name,
         e.exercise_id AS current_id,
+        e.exercise_parent_id AS parent_id,
         0 AS depth
     FROM {{ ref('dim_exercises') }} e
+    WHERE e.exercise_parent_id IS NULL -- gets depth 0
 
     UNION ALL
 
     SELECT
         'exercise' AS dimension,
-        c.ancestor_name,
-        c.ancestor_id,
+        e.exercise_name AS current_name,
         e.exercise_id AS current_id,
+        e.exercise_parent_id AS parent_id,
         c.depth + 1
     FROM exercise_tree c
     JOIN {{ ref('dim_exercises') }} e
@@ -28,19 +29,20 @@ WITH RECURSIVE exercise_tree AS (
 muscle_tree AS (
     SELECT
         'muscle' AS dimension,
-        m.muscle_name AS ancestor_name,
-        m.muscle_id AS ancestor_id,
+        m.muscle_name AS current_name,
         m.muscle_id AS current_id,
+        m.muscle_parent_id AS parent_id,
         0 AS depth
     FROM {{ ref('dim_muscles') }} m
+    WHERE m.muscle_parent_id IS NULL
 
     UNION ALL
 
     SELECT
         'muscle' AS dimension,
-        c.ancestor_name,
-        c.ancestor_id,
+        m.muscle_name AS current_name,
         m.muscle_id AS current_id,
+        m.muscle_parent_id AS parent_id,
         c.depth + 1
     FROM muscle_tree c
     JOIN {{ ref('dim_muscles') }} m
@@ -50,19 +52,20 @@ muscle_tree AS (
 attachment_tree AS (
     SELECT
         'attachment' AS dimension,
-        a.attachment_name AS ancestor_name,
-        a.attachment_id AS ancestor_id,
+        a.attachment_name AS current_name,
         a.attachment_id AS current_id,
+        a.attachment_parent_id AS parent_id,
         0 AS depth
     FROM {{ ref('dim_attachments') }} a
+    WHERE a.attachment_parent_id IS NULL
 
     UNION ALL
 
     SELECT
         'attachment' AS dimension,
-        c.ancestor_name,
-        c.ancestor_id,
+        a.attachment_name AS current_name,
         a.attachment_id AS current_id,
+        a.attachment_parent_id AS parent_id,
         c.depth + 1
     FROM attachment_tree c
     JOIN {{ ref('dim_attachments') }} a
@@ -72,33 +75,34 @@ attachment_tree AS (
 equipment_tree AS (
     SELECT
         'equipment' AS dimension,
-        eq.equipment_name AS ancestor_name,
-        eq.equipment_id AS ancestor_id,
+        eq.equipment_name AS current_name,
         eq.equipment_id AS current_id,
+        eq.equipment_parent_id AS parent_id,
         0 AS depth
     FROM {{ ref('dim_equipment') }} eq
+    WHERE eq.equipment_parent_id IS NULL
 
     UNION ALL
 
     SELECT
         'equipment' AS dimension,
-        c.ancestor_name,
-        c.ancestor_id,
+        eq.equipment_name AS current_name,
         eq.equipment_id AS current_id,
+        eq.equipment_parent_id AS parent_id,
         c.depth + 1
     FROM equipment_tree c
     JOIN {{ ref('dim_equipment') }} eq
       ON eq.equipment_parent_id = c.current_id
 )
 
-SELECT dimension, ancestor_name, ancestor_id, current_id, depth
+SELECT dimension, current_name, current_id, parent_id, depth
 FROM exercise_tree
 UNION ALL
-SELECT dimension, ancestor_name, ancestor_id, current_id, depth
+SELECT dimension, current_name, current_id, parent_id, depth
 FROM muscle_tree
 UNION ALL
-SELECT dimension, ancestor_name, ancestor_id, current_id, depth
+SELECT dimension, current_name, current_id, parent_id, depth
 FROM attachment_tree
 UNION ALL
-SELECT dimension, ancestor_name, ancestor_id, current_id, depth
+SELECT dimension, current_name, current_id, parent_id, depth
 FROM equipment_tree
